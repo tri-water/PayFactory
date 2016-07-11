99bf8fadc8cec932f8bf9c10a625f315c31a7353
Xiangming Gao <gao_xiangming@hotmail.com>
2016-07-11 23:08:17 +0100

    Add some comments in PayOffFactory.h

--- PayFactory/PayOffFactory.h
+++ PayFactory/PayOffFactory.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 
+// Singleton pattern
 class PayOffFactory
 {
 public:
@@ -10,6 +11,7 @@ public:
 	// which take in a double and spit out a PayOff* as CreatePayOffFunction
 	typedef PayOff* (*CreatePayOffFunction)(double);
 
+	// a static function returns a static object, which will only be constructed once
 	static PayOffFactory& Instance();
 	void RegisterPayOff(std::string, CreatePayOffFunction);
 	PayOff* CreatePayOff(std::string PayOffID, double Strike);
@@ -18,6 +20,8 @@ public:
 private:
 	std::map<std::string, CreatePayOffFunction> TheCreatorFunctions;
 	PayOffFactory() {}
+	// Keep the constructor and assignment function private
+	// only allowing it called by static function Instance
 	PayOffFactory(const PayOffFactory&) {}
 	PayOffFactory& operator = (const PayOffFactory) { return *this; }
 };
